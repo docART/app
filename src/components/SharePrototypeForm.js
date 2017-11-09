ca123776d1b5890f5da3f32a6a8811a74f8f308f
Stefan Freudenberg <stefan@agaric.com>
2017-11-09 17:05:48 +0100

    Update form for sharing prototype

--- src/components/SharePrototypeForm.js
+++ src/components/SharePrototypeForm.js
@@ -5,14 +5,21 @@ let SharePrototypeForm = (props) => {
   const { handleSubmit } = props;
   return (
     <form onSubmit={ handleSubmit }>
-        <Field name="name" component="input" type="text" placeholder="Título" maxLength="256" required="" className="darker field w-input" />
-        <Field name="nick" component="input" type="text" placeholder="Apodo" maxLength="256" required="" className="darker field w-input" />
-        <Field name="summary" component="textarea" placeholder="Resumen" required="" className="darker field w-input" />
-        <Field name="motivations" component="textarea" placeholder="Motivaciones" required="" className="darker field w-input" />
-        <Field name="team" component="textarea" type="" placeholder="Equipo" required="" className="darker field w-input" />
-        <Field name="promoter" component="input" type="text" placeholder="Promotor" maxLength="256" required="" className="darker field w-input" />
-        <Field name="email" component="input" type="text" placeholder="Correo Electrónico" maxLength="256" required="" className="darker field w-input" />
-        <Field name="license" component="select" required="" className="darker field w-input" />
+        <Field name="title" component="input" type="text" placeholder="Título" maxLength="256" required className="darker field w-input" />
+        <Field name="nick" component="input" type="text" placeholder="Apodo" maxLength="256" required className="darker field w-input" />
+        <Field name="summary" component="textarea" placeholder="Resumen" className="darker field w-input" />
+        <Field name="motivations" component="textarea" placeholder="Motivaciones" className="darker field w-input" />
+        <Field name="team" component="textarea" type="" placeholder="Equipo" className="darker field w-input" />
+        <Field name="promoter" component="input" type="text" placeholder="Promotor" maxLength="256" className="darker field w-input" />
+        <Field name="email" component="input" type="text" placeholder="Correo electrónico" maxLength="256" className="darker field w-input" />
+        <Field name="license" component="select" className="darker field w-input">
+            <option></option>
+            <option>GNU Free Documentation License</option>
+            <option>Creative Commons Zero v1.0 Universal</option>
+            <option>Creative Commons Attribution 4.0</option>
+            <option>Creative Commons Attribution Share Alike 4.0</option>
+        </Field>
+        <button className="button" type="submit">Dale</button>
     </form>
   );
 };
