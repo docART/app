6e21f2f8e88271f8477e1cfd33638b52e3b11513
Stefan Freudenberg <stefan@agaric.com>
2017-11-14 20:40:25 +0100

    Prevent double postings

--- src/components/modules/PrototypeWizardPage2.js
+++ src/components/modules/PrototypeWizardPage2.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { createPrototypeRequested } from '../../actions';
 
 let PrototypeWizardPage2 = (props) => {
-  const { handleSubmit } = props;
+  const { handleSubmit, isPosting } = props;
   return (
     <form onSubmit={ handleSubmit }>
         <Field name="procedure" component="textarea" placeholder="Procedimiento"  required className="darker field w-input" />
@@ -12,17 +13,20 @@ let PrototypeWizardPage2 = (props) => {
         <Field name="references" component="textarea" placeholder="Referencias"  required className="darker field w-input" />
         <Field name="logo" component="input" type="url" placeholder="Logo" className="darker field w-input" />
         <Field name="video" component="input" type="url" placeholder="Video" maxLength="256" className="darker field w-input" />
-
-        <button className="button form w-button" type="submit">Dale</button>
+        <button className="button form w-button" type="submit" disabled={isPosting}>Dale</button>
     </form>
   );
 };
 
-export default reduxForm({
+const mapStateToProps = (state) => ({
+   isPosting: state.prototypes.isPosting
+});
+
+export default connect(mapStateToProps)(reduxForm({
     form: 'share prototype',
     destroyOnUnmount: false,
     forceUnregisterOnUnmount: true,
     onSubmit: (values, dispatch) => {
       dispatch(createPrototypeRequested(values));
     }
-})(PrototypeWizardPage2);
+})(PrototypeWizardPage2));
