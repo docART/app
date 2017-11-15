c2c817620d819d88cccd0b3359622420ae4bc4e2
Juan <jdafcorp2008@gmail.com>
2017-11-15 21:12:05 +0100

    future form modified

--- src/components/modules/FutureForm.js
+++ src/components/modules/FutureForm.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { saveDocumentRequested } from '../../actions';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const FutureForm = (props) => {
     const { handleSubmit } = props;
     return (
-    <form onSubmit={ handleSubmit }>
+    <Form onSubmit={ handleSubmit }>
         <Field name="prototype" component="input" type="hidden"/>
         <Field name="path" component="input" type="hidden"/>
         <Field name="section" component="input" type="hidden"/>
@@ -16,20 +17,45 @@ const FutureForm = (props) => {
             Este es el momento para recomendar algunas referencias: videos, blogs, libros, artículos, redes, informe,..
             Quizás lo más importante sea dedicar un tiempo para indicar desarrollos futuros, formas de uso, alianzas estratégicas,...
         </p>
-        <Field name="share" component="textarea" placeholder="Compartir" required className="darker field w-input" />
-        <label htmlFor="share">¿Cómo se comparte el prototipo? </label>
-        <Field name="future_development" component="textarea" placeholder="Desarrollo Futuro" required className="darker field w-input" />
-        <label htmlFor="future_development">¿Hay alguna estrategia para continuar?</label>
-        <Field name="maintenance" component="textarea" placeholder="Mantenimiento" required className="darker field w-input" />
-        <label htmlFor="maintenance">¿Qué cosas hay que hacer para su mantenimiento?</label>
-        <Field name="references" component="textarea" placeholder="Referencias" required className="darker field w-input" />
-        <label htmlFor="references" >¿Qué referencias deberían tomar en cuenta los interesados?</label>
-        <Field name="image" component="input" type="url" placeholder="Url de la imagen" required className="darker field w-input" />
-        <label htmlFor="image" >¿Qué imagen quieres compartir?</label>
-        <Field name="imagesreferences" component="textarea" placeholder="Referencias de la imagen" required className="darker field w-input" />
-        <label htmlFor="image" >¿No olvides la referencia de la imagen?</label>
-        <button type="submit" className="button form w-button">Guardar</button>
-    </form>
+        <FormGroup>
+            <label htmlFor="share">Compartir</label>
+            <Field name="share" id="share" component={Input} type="textarea" placeholder="Compartir" required/>
+            <Field htmlFor="share" component={ FormText }>¿Como se comparte el prototipo?</Field>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="future_development">Desarrollo futuro</label>
+            <Field name="future_development" id="future_development" component={Input} type="textarea" placeholder="Desarrollo futuro" required/>
+            <Field htmlFor="future_development" component={ FormText }>¿Hay alguna estrategia para continuar?</Field>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="maintenance">Mantenimiento</label>
+            <Field name="maintenance" id="maintenance" component={Input} type="textarea" placeholder="Mantenimiento" required/>
+            <Field htmlFor="maintenance" component={ FormText }>¿Qué cosas hay que hacer para su mantenimiento?</Field>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="references">Referencias</label>
+            <Field name="references" id="references" component={Input} type="textarea" placeholder="Referencias" required/>
+            <Field htmlFor="references" component={ FormText }>¿Qué referencias deberían tomar en cuenta los interesados?</Field>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="image">Url de la imagen</label>
+            <Field name="image" id="image" component={Input} type="textarea" placeholder="Url de la imagen" required/>
+            <Field htmlFor="image" component={ FormText }>¿Qué imagen quieres compartir?</Field>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="image">Url de la imagen</label>
+            <Field name="image" id="image" component={Input} type="textarea" placeholder="Url de la imagen" required/>
+            <Field htmlFor="image" component={ FormText }>¿Qué imagen quieres compartir?</Field>
+        </FormGroup>
+        <FormGroup>
+            <label htmlFor="imagesreferences">Referencias de la imagen</label>
+            <Field name="imagesreferences" id="imagesreferences" component={Input} type="textarea" placeholder="Referencias de la imagen" required/>
+            <Field htmlFor="imagesreferences" component={ FormText }>¿No olvides la referencia de la imagen?</Field>
+        <FormGroup>
+        </FormGroup>
+            <Field component={Button} type="submit">Update</Field>
+        </FormGroup> 
+     </Form>
     );
 };
 
