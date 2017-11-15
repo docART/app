7520c5bd50fd445afa23fc74b55cfbf1af423505
Stefan Freudenberg <stefan@agaric.com>
2017-11-15 11:49:01 +0100

    Add missing file

---
+++ src/components/pages/PrototypeRecipe.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
+} from 'react-accessible-accordion';
+import ReactMarkdown from 'react-markdown';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Navbar from '../modules/Navbar';
+import { fetchDocumentsRequested } from '../../actions';
+
+import '../../../node_modules/react-accessible-accordion/dist/react-accessible-accordion.css';
+
+class PrototypeRecipe extends Component {
+
+    componentWillMount = () => {
+        this.props.fetchDocumentsRequested(this.props.match.params.name);
+    }
+
+    render = () => {
+        const { documents, match } = this.props;
+        const departure = [];
+        const prototyping = [];
+        const future = [];
+        if (documents[match.params.name]) {
+            Object.entries(documents[match.params.name].items).map((currentValue) => {
+                if (currentValue[0].endsWith('README.md')) {
+                    return;
+                }
+                if (currentValue[0].startsWith('departure')) {
+                    departure.push(currentValue[1]);
+                } else if (currentValue[0].startsWith('prototyping')) {
+                    prototyping.push(currentValue[1]);
+                } else if (currentValue[0].startsWith('future')) {
+                    future.push(currentValue[1]);
+                }
+            });
+        }
+        return (
+            <div>
+                <Navbar match={match}/>
+                <div className="section">
+                    <div className="w-container">
+                        <Accordion>
+                            <AccordionItem>
+                                <AccordionItemTitle>
+                                    <h2>Antes</h2>
+                                </AccordionItemTitle>
+                                <AccordionItemBody>
+                                    <div>
+                                        {departure.map((currentValue, index) => (
+                                            <ReactMarkdown key={index} source={currentValue}/>
+                                        ))}
+                                        <p>
+                                            <Link to={`/prototypes/${match.params.name}/recipes/departure`} className="button">+</Link>
+                                        </p>
+                                    </div>
+                                </AccordionItemBody>
+                            </AccordionItem>
+                            <AccordionItem>
+                                <AccordionItemTitle>
+                                    <h2>Durante</h2>
+                                </AccordionItemTitle>
+                                <AccordionItemBody>
+                                    <div>
+                                        {prototyping.map((currentValue, index) => (
+                                            <ReactMarkdown key={index} source={currentValue}/>
+                                        ))}
+                                        <p>
+                                            <Link to={`/prototypes/${match.params.name}/recipes/prototyping`} className="button">+</Link>
+                                        </p>
+                                    </div>
+                                </AccordionItemBody>
+                            </AccordionItem>
+                            <AccordionItem>
+                                <AccordionItemTitle>
+                                    <h2>Después</h2>
+                                </AccordionItemTitle>
+                                <AccordionItemBody>
+                                    <div>
+                                        {future.map((currentValue, index) => (
+                                            <ReactMarkdown key={index} source={currentValue}/>
+                                        ))}
+                                        <p>
+                                            <Link to={`/prototypes/${match.params.name}/recipes/departure`} className="button">+</Link>
+                                        </p>
+                                    </div>
+                                </AccordionItemBody>
+                            </AccordionItem>
+                        </Accordion>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+const mapStateToProps = (state) => {
+    return {
+        documents: state.documents
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    { fetchDocumentsRequested }
+)(PrototypeRecipe);
