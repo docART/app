a8bce8a175eeff2630268651c521434a7a1ee9f8
Stefan Freudenberg <stefan@agaric.com>
2017-11-25 15:35:34 +0100

    Format source code and replace brand link

--- src/components/modules/Navbar.js
+++ src/components/modules/Navbar.js
@@ -1,54 +1,76 @@
-import React from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import {Collapse, Navbar, NavbarToggler, Nav, NavItem} from 'reactstrap';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
+class Header extends Component {
+    constructor(props) {
+        super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-  render() {
-    const links = [
-        <NavItem  key="home"><NavLink exact to="/" className="nav-link" activeClassName="active">←  Inicio</NavLink></NavItem>
-    ];
-    const { match } = this.props;
-    switch (match.path) {
-        case '/':
-            links.push(<NavItem  key="form"><NavLink exact to="/prototype/form" className="nav-link" activeClassName="active">Compartir Prototipo</NavLink></NavItem>);
-            break;
-        case '/prototypes/:name':
-        case '/prototypes/:name/long':
-        case '/prototypes/:name/recipes/:section/:file?':
-            links.push(<NavItem key="quick"><NavLink exact to={`/prototypes/${match.params.name}`} className="nav-link" activeClassName="active">Receta rápida</NavLink></NavItem>)
-            links.push(<NavItem key="long"><NavLink exact to={`/prototypes/${match.params.name}/long`} className="nav-link" activeClassName="active">Receta lenta</NavLink></NavItem>)
-            links.push(<NavItem key="insights"><NavLink exact to={`/prototypes/${match.params.name}/insights`} className="nav-link" activeClassName="active">Mapa de aprendizaje</NavLink></NavItem>)
-            break;
-        default:
-            break;
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
     }
-    return (
-        <Navbar className="bg-light" fixed="top" light color="faded" expand="md">
-            <div className="container">
-                <NavbarBrand href="/">docART</NavbarBrand>
-                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                <Collapse isOpen={this.state.collapsed} navbar>
-                    <Nav className="ml-auto" navbar>
-                        {links}
-                    </Nav>
-                </Collapse>
-            </div>
-        </Navbar>
-    );
-  }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    render() {
+        const {match} = this.props;
+        const links = [
+            <NavItem key="home">
+                <NavLink exact to="/" className="nav-link">← Inicio</NavLink>
+            </NavItem>
+        ];
+
+        switch (match.path) {
+            case '/':
+                links.push(
+                    <NavItem key="form">
+                        <NavLink exact to="/prototype/form" className="nav-link">Compartir Prototipo</NavLink>
+                    </NavItem>
+                );
+                break;
+            case '/prototypes/:name':
+            case '/prototypes/:name/long':
+            case '/prototypes/:name/recipes/:section/:file?':
+                links.push(
+                    <NavItem key="quick">
+                        <NavLink exact to={`/prototypes/${match.params.name}`} className="nav-link">Receta
+                    rápida</NavLink></NavItem>
+                );
+                links.push(
+                    <NavItem key="long">
+                        <NavLink exact to={`/prototypes/${match.params.name}/long`} className="nav-link">Receta lenta</NavLink>
+                    </NavItem>
+                );
+                links.push(
+                    <NavItem key="insights">
+                        <NavLink exact to={`/prototypes/${match.params.name}/insights`} className="nav-link">Mapade aprendizaje</NavLink>
+                    </NavItem>
+                );
+                break;
+            default:
+                break;
+        }
+        return (
+            <Navbar className="bg-light" fixed="top" light color="faded" expand="md">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">docART</Link>
+                    <NavbarToggler onClick={this.toggleNavbar}
+                                   className="mr-2"/>
+                    <Collapse isOpen={this.state.collapsed} navbar>
+                        <Nav className="ml-auto" navbar>
+                            {links}
+                        </Nav>
+                    </Collapse>
+                </div>
+            </Navbar>
+        );
+    }
 }
 
 export default Header;
