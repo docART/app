33fec7629c3f456580f847dec2aa875b4cf19cb9
Stefan Freudenberg <stefan@agaric.com>
2017-11-25 15:14:18 +0100

    Improve footer styling

--- src/components/modules/Footer.js
+++ src/components/modules/Footer.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer id="footer" className="footer section">
-        <div className="w-container">
-            <p className="footer-text">Footer. Licencia Creative commons - 2017</p>
-        </div>
+    <footer className="mt-5">
+        <p className="text-center">Footer. Licencia Creative commons - 2017</p>
     </footer>
 );
 
